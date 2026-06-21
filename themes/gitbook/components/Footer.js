9b98ade1336434c747f5415858758be13c04e993
rain9527-001 <raintest9527@gmail.com>
2026-06-21 13:26:10 +0800

    修改页脚
    Replaced SocialButton with a link to the about page represented by a house icon. Updated footer content and styling.

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -1,6 +1,5 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
 
 /**
  * 站点页脚
@@ -16,7 +15,13 @@ const Footer = ({ siteInfo }) => {
 
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
-      <SocialButton />
+      
+      {/* 替换了原本的 SocialButton，改成了指向 about 的小房子图标 */}
+      <div className='flex justify-center pb-1'>
+        <a href='/about' title='关于我' className='text-gray-500 hover:text-black dark:text-gray-400 dark:hover:text-white transition-colors text-lg'>
+          <i className='fas fa-home' />
+        </a>
+      </div>
 
       {/* 极简自定义版权信息，去除了爱心和多余的链接 */}
       <div className='flex justify-center py-1 text-gray-500 dark:text-gray-400'>
