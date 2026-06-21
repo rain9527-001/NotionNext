d9fc607671d659f5bbefe56ff7c789bc9f8fefd6
rain9527-001 <raintest9527@gmail.com>
2026-06-21 12:12:14 +0800

    修改侧边栏排序
    Sort categories by name in ascending order before rendering.

--- themes/gitbook/components/CategoryGroup.js
+++ themes/gitbook/components/CategoryGroup.js
@@ -1,14 +1,20 @@
-
 import CategoryItem from './CategoryItem'
 
 const CategoryGroup = ({ currentCategory, categoryOptions }) => {
   if (!categoryOptions) {
     return <></>
   }
+
+  // 在这里对分类进行排序：按中文/字母/数字顺序升序排列
+  const sortedCategories = [...categoryOptions].sort((a, b) =>
+    a.name.localeCompare(b.name, 'zh-CN', { numeric: true })
+  )
+
   return <div id='category-list' className='pt-4'>
     <div className='mb-2'><i className='mr-2 fas fa-th' />分类</div>
     <div className='flex flex-wrap'>
-      {categoryOptions?.map(category => {
+      {/* 将原来的 categoryOptions 改为排序后的 sortedCategories */}
+      {sortedCategories?.map(category => {
         const selected = currentCategory === category.name
         return <CategoryItem key={category.name} selected={selected} category={category.name} categoryCount={category.count} />
       })}
