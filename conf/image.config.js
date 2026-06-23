d289e4d4ec025e489a0d849360d43e3d4d925254
rain9527-001 <raintest9527@gmail.com>
2026-06-23 21:48:34 +0800

    换行

--- conf/image.config.js
+++ conf/image.config.js
@@ -16,6 +16,7 @@ module.exports = {
 
   // 网站图片
   
+  
   IMG_LAZY_LOAD_PLACEHOLDER:
     process.env.NEXT_PUBLIC_IMG_LAZY_LOAD_PLACEHOLDER ||
     'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', // 懒加载占位图片地址，支持base64或url
